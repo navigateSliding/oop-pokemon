ad399851448b22072bc8c7149c139ead7e5242c4
ChrLos <lindsey.elizabeta.a4mzm@slmail.me>
2025-08-07 00:40:53 -0400

    fix: Fixing Score Dup of Same Player

--- pokemon/src/main/java/dev/project/pokemon/battle/ScoreManager.java
+++ pokemon/src/main/java/dev/project/pokemon/battle/ScoreManager.java
@@ -15,11 +15,32 @@ public class ScoreManager {
         this.database = database;
         this.highScores = database.loadScores();
     }
-    
+
     public void addScore(String playerName, int score) {
-        ScoreEntry newScore = new ScoreEntry(playerName, score);
-        highScores.add(newScore);
-        
+        // Check if player already exists in high scores
+        boolean playerExists = false;
+        for (int i = 0; i < highScores.size(); i++) {
+            ScoreEntry existingEntry = highScores.get(i);
+            if (existingEntry.getPlayerName().equals(playerName)) {
+                // Player exists - update only if new score is higher
+                if (score > existingEntry.getScore()) {
+                    highScores.set(i, new ScoreEntry(playerName, score));
+                    System.out.println("Updated high score for " + playerName + ": " + score + " points (previous: " + existingEntry.getScore() + ")");
+                } else {
+                    System.out.println(playerName + "'s current score (" + score + ") is not higher than their best (" + existingEntry.getScore() + ")");
+                }
+                playerExists = true;
+                break;
+            }
+        }
+
+        // If player doesn't exist, add new entry
+        if (!playerExists) {
+            ScoreEntry newScore = new ScoreEntry(playerName, score);
+            highScores.add(newScore);
+            System.out.println("New high score added: " + newScore);
+        }
+
         // Sort scores in descending order
         Collections.sort(highScores, new Comparator<ScoreEntry>() {
             @Override
@@ -27,16 +48,14 @@ public class ScoreManager {
                 return Integer.compare(b.getScore(), a.getScore());
             }
         });
-        
+
         // Keep only top 5 scores
         if (highScores.size() > MAX_HIGH_SCORES) {
             highScores = new ArrayList<>(highScores.subList(0, MAX_HIGH_SCORES));
         }
-        
+
         // Save to file
         database.saveScores(highScores);
-        
-        System.out.println("Score added: " + newScore);
     }
     
     public ArrayList<ScoreEntry> getTopScores() {
